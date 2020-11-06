1a6ed9d682d6e81690df3a649dc380ecf3bf10f3
Phaniraj B.N <phanirajbn@yahoo.in>
2020-11-06 10:31:33 +0530

    FirstExample is Commited

---
+++ firstExample.java
@@ -0,0 +1,7 @@
+class firstExample{
+    public static void main(String [] args) {
+        System.out.println("Hello There!!!!");
+    }
+}
+//git add firstExample.java : adds the file into the local repostory
+//git status: will get the current list of the files that has modified since the last update. 
